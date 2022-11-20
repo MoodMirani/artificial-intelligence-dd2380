19c40e3386a85bf3c8884153ebdd5f4d0b20bad3
Mahmoud Sherzad <47563174+MoodSherzad@users.noreply.github.com>
2022-11-20 23:10:12 +0100

    progress

--- README.md
+++
@@ -1,1 +0,0 @@
-# artificial-intelligence-dd2380
--- Search/player.py
+++ Search/player.py
@@ -1,12 +1,12 @@
-#!/usr/bin/env python3
 import random
 import math
 import time
 
+# from operator import itemgetter
+
 from fishing_game_core.game_tree import Node
 from fishing_game_core.player_utils import PlayerController
 from fishing_game_core.shared import ACTION_TO_STR
-
 
 class PlayerControllerHuman(PlayerController):
     def player_loop(self):
@@ -25,11 +25,12 @@ class PlayerControllerHuman(PlayerController):
             if msg["game_over"]:
                 return
 
-
 class PlayerControllerMinimax(PlayerController):
 
     def __init__(self):
+        self.repeated_states_dict = {}
         super(PlayerControllerMinimax, self).__init__()
+
 
     def player_loop(self):
         """
@@ -41,57 +42,19 @@ class PlayerControllerMinimax(PlayerController):
         first_msg = self.receiver()
 
         while True:
+
             msg = self.receiver()
 
             # Create the root node of the game tree
             node = Node(message=msg, player=0)
 
             # Possible next moves: "stay", "left", "right", "up", "down"
-            best_move = self.search_best_next_move(initial_tree_node=node)
+            bestMove = self.search_best_next_move(initial_tree_node=node, player=0)
 
             # Execute next action
-            self.sender({"action": best_move, "search_time": None})
+            self.sender({"action": bestMove, "search_time": None})
 
-    def heuristic(self, initial_tree_node): #l1
-        totalScore = initial_tree_node.state.player_scores[0] - initial_tree_node.state.player_scores[1]
-        h = 0
-        for i in initial_tree_node.fish_positions:
-            distance = min(abs(initial_tree_node.fish_positions[0] - initial_tree_node.hook_position[0]), 20- abs(initial_tree_node.fish_positions[0] - initial_tree_node.hook_position[0])) + abs(initial_tree_node.fish_positions[1]- initial_tree_node.hook_position[1])
-            if distance == 0 and initial_tree_node.fish_scores[i] > 0:
-                return math.inf
-            h = max(h, initial_tree_node.fish_scores[i]* math.exp(-distance))
-        return 2 * totalScore + h
-
-    def ab_minimax(self, initial_tree_node, state, depth, alpha, beta, player, startTime):
-        if time.time() - startTime > 0.075:
-            raise TimeoutError
-        else:
-            children = initial_tree_node.compute_and_get_children()
-            if depth == 0 or len(children) == 0:
-                return self.heuristic(initial_tree_node)
-
-            elif player == 0:
-                maxEval = -math.inf
-                for child in state:
-                    eval = self.ab_minimax(child, child.state, depth - 1, alpha, beta, player=1, startTime=startTime)
-                    maxEval = max(maxEval, eval)
-                    alpha = max(alpha, eval)
-                    if beta <= alpha:
-                        break
-                return maxEval
-            else:
-
-                minEval = +math.inf
-                for child in state:
-                    eval = self.ab_minimax(child, child.state, depth - 1, alpha, beta, player=0, startTime=startTime)
-                    minEval = min(minEval, eval)
-                    beta = min(beta, eval)
-                    if beta <= alpha:
-                        break
-                return minEval
-
-
-    def search_best_next_move(self, initial_tree_node):
+    def search_best_next_move(self, initial_tree_node, player):
         """
         Use minimax (and extensions) to find best possible next move for player 0 (green boat)
         :param initial_tree_node: Initial game tree node
@@ -104,28 +67,118 @@ class PlayerControllerMinimax(PlayerController):
         # EDIT THIS METHOD TO RETURN BEST NEXT POSSIBLE MODE USING MINIMAX ###
 
         # NOTE: Don't forget to initialize the children of the current node
-        #       with its compute_and_get_children() method!
+        #       with its compute_and_get_children() method! 
+        # compute_and_get_children() returns a list of the child Nodes, found one level below in the game tree.
+
         startTime = time.time()
+        timeLimit = 0.07
         timeout = False
         alpha = -math.inf
         beta = math.inf
-        children = initial_tree_node.compute_and_get_children()
-        scores = []
+        startValue = -math.inf                               
         depth = 0
-        best_move = 0
+        bestMove = 0
+        children = initial_tree_node.compute_and_get_children()
+
+    
+        # children.sort(key=self.heuristic_eval, reverse=True)
+
+        # IDS
         while not timeout:
             try:
                 for child in children:
-                    print('hej')
-                    score = self.ab_minimax(child, child.state, depth, alpha, beta, 1, startTime)
-                    scores.append(score)
-                    depth += 1
-                best_score = scores.index(max(scores))
-                move = children[best_score].move
-
-                best_move = move
+                    value = self.minimax(child, player, depth, alpha, beta, startTime, timeLimit)
+                    if value > startValue:
+                        startValue = value
+                        bestMove = child.move
+                depth += 1
+                if (time.time() - startTime) > timeLimit:
+                    raise TimeoutError
             except:
                 timeout = True
+        return ACTION_TO_STR[bestMove]
 
-        random_move = random.randrange(5)
-        return ACTION_TO_STR[best_move]
+
+    def hash(self, state):
+        '''
+        Computes the string hash of a given state, by describing state in full
+        :param state:
+        :return: hashed string of state
+        '''
+        string = str(state.get_player_scores()) + str(state.get_fish_positions()) + str(state.get_hook_positions())
+        hashed_string = hash(string) # 32 bit istället 64 bit
+        return hashed_string
+
+    def minimax(self, node, player, depth, alpha, beta, startTime, timeLimit):
+        '''
+        MINIMAX FUNCTION with alpha-beta pruning
+        :param node: node that we are analysing
+        :param player: player that is playing and decides if we want to find min or max
+        :param depth: depth of node
+        :param alpha:
+        :param beta:
+        :param startTime:
+        :param timeLimit:
+        :return: a heuristic value that approximates a utility function of the state
+        '''
+
+        if (time.time() - startTime) > timeLimit:
+            raise TimeoutError
+
+        # Repeated state check
+        key = self.hash(node.state)
+        if key in self.repeated_states_dict and self.repeated_states_dict[key][0] >= depth:
+            return self.repeated_states_dict[key][1]
+
+        # move ordering
+        children = node.compute_and_get_children()
+        children.sort(key=self.heuristic_eval, reverse=True)
+
+
+        if (time.time() - startTime) > timeLimit or len(children) == 0 or depth == 0:
+            return self.heuristic_eval(node)
+
+        elif player == 0:
+            v = float('-inf')
+            for child in children:
+                v = max(v, self.minimax(child, 1, depth - 1, alpha, beta, startTime, timeLimit))
+                alpha = max(alpha, v)
+                if (time.time() - startTime) > timeLimit or beta <= alpha:
+                    return v
+
+        elif player == 1:
+            v = float('inf')
+            for child in reversed(children):
+                v = min(v, self.minimax(child, 0, depth - 1, alpha, beta, startTime, timeLimit))
+                beta = min(beta, v)
+                if (time.time() - startTime) > timeLimit or beta <= alpha:
+                    return v
+
+
+        self.repeated_states_dict.update({key: [depth, v]})
+        return v
+
+    def heuristic_eval(self, node):
+        '''
+        :param node: node
+        :return: returns score
+        '''
+        player = node.state.player
+        player_score, opponent_score = node.state.get_player_scores() # Spelarnas poäng
+
+        score = player_score - opponent_score
+        fish_scores = node.state.get_fish_scores()                  # Fiskarnas score
+        fish_positions = node.state.get_fish_positions()            # Fiskarnas position
+        hook_position = node.state.get_hook_positions()[player]     # krokposition för player
+
+        for fish_index, fish_pos in fish_positions.items():
+            distance = abs(fish_pos[0] - hook_position[0]) + abs(fish_pos[1] - hook_position[1]) # manhattan
+            if distance == 0:                                                                    # distance == 0, fångade fisk
+                score += (1 - (player * 2))*(fish_scores[fish_index])*10                         # fångade fiskar väger mer
+            else:
+                score += (((1 - (player * 2))*(fish_scores[fish_index]) / (distance)))     # ej delat med 0, vid player 0 = +, player 1 = -
+
+        return score
+
+
+
